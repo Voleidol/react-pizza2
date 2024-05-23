b7df2c7d023cd83af699c970cd09550d4585036e
Voleidol <fhdha@yandex.ru>
2024-05-23 14:38:21 +0300

    add 3 lesson

---
+++ src/components/Categories.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function Categories() {
+
+  const [active, setActive] = useState(false);
+
+    return (
+      <div className="categories">
+        <ul>
+          <li className="active">Все</li>
+          <li>Мясные</li>
+          <li>Вегетарианская</li>
+          <li>Гриль</li>
+          <li>Острые</li>
+          <li>Закрытые</li>
+        </ul>
+      </div>
+    );
+  }
+
+  export default Categories;
