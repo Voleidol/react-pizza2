a2c1e0d8b93630b8bab52f38a1678a415214da67
Roman <fhdha@yandex.ru>
2024-05-26 18:44:26 +0300

    add

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {pizzas.map((obj) => (
-              <PizzaBlock title={obj.title} price={obj.price} />
+              <PizzaBlock title={obj.title} price={obj.price} image={obj.imageUrl} />
             ))}
           </div>
         </div>
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-function PizzaBlock({ title, price }) {
+function PizzaBlock({ title, price, image }) {
   const [pizzaCount, setPizzaCount] = useState(0);
 
   const onClickAdd = () => {
@@ -12,7 +12,8 @@ function PizzaBlock({ title, price }) {
       <div className="pizza-block">
         <img
           className="pizza-block__image"
-          src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+          src={image}
+          // src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
           alt="Pizza"
         />
         <h4 className="pizza-block__title">{title}</h4>
