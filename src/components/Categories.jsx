3581d7f70f9dfcbf32a8211ccc755b52d3724b6f
Voleidol <fhdha@yandex.ru>
2024-05-30 15:10:57 +0300

    Добавил сортировку и категории общий стейт

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,22 +1,16 @@
-import { useState } from "react";
+import React from 'react'
 
-function Categories({value, onClickCategory}) {
+function Categories({value, onChangeCategory}) {
 
   const categoriesMass = [
     'Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'
   ];
-  
-  // const [activeIndex, setActiveIndex] = useState(0);
-  
-  // const onClickCategories = (index) => {
-  //   setActiveIndex(index);
-  // };
 
     return (
       <div className="categories">
         <ul>
           {categoriesMass.map((item, index) => (
-            <li key={index} onClick={() => onClickCategory(index)} className={value === index ? 'active' : ''}>{item}</li>
+            <li key={index} onClick={() => onChangeCategory(index)} className={value === index ? 'active' : ''}>{item}</li>
           ))}
         </ul>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,23 +11,30 @@ export default function Home() {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
-  const [sortTypel, setSortTypel] = useState(0);
+  const [sortType, setSortType] = useState({ name: "популярности", sortProperty: "rating" });
 
   useEffect(() => {
-    fetch('https://632d6dfe0d7928c7d24ae553.mockapi.io/items')
-    .then((res) => res.json())
-    .then((arr) => {
+    setIsLoading(true);
+
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const sortBy = sortType.sortProperty.replace('-', '');
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    fetch(
+      `https://632d6dfe0d7928c7d24ae553.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+    )
+      .then((res) => res.json())
+      .then((arr) => {
         setItems(arr);
         setIsLoading(false);
-    });
+      });
     window.scrollTo(0, 0);
-  }, []);
+  }, [categoryId, sortType]);
 
   return (
     <div className="container">
       <div className="content__top">
-            <Categories value={categoryId} onClickCategory={setCategoryId}/>
-            <Sort />
+            <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)}/>
+            <Sort value={sortType} onChangeSort={(i) => setSortType(i)}/>
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
