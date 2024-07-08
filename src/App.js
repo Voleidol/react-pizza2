3c2384d304b2fd6b132d711118777a8bbf382092
Voleidol <fhdha@yandex.ru>
2024-07-08 15:34:03 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { Route, Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Home from './pages/Home';
-import './scss/app.scss';
-import NotFound from './pages/NotFound';
 import Cart from './pages/Cart';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import './scss/app.scss';
 
 export const SearchContext = React.createContext({});
 
@@ -13,14 +13,14 @@ function App() {
 
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{searchValue, setSearchValue}}>
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
-            <Routes>
-              <Route path='/' element={<Home />}/>
-              <Route path='/cart' element={<Cart />}/>
-              <Route path='*' element={<NotFound />}/>
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </SearchContext.Provider>
     </div>
